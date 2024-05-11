6015ba685e445fec4d80381ec5f1c7d4e047078e
tikhanovitch <tikhanovitch@gmail.com>
2024-05-11 23:01:29 +0300

    Create all models, make migration and register in admin

--- src/booking_app/admin.py
+++ src/booking_app/admin.py
@@ -1,3 +1,18 @@
+from .models import (
+    User, Person, HotelOwner,
+    Profile, Hotel, BookInfo,
+    HotelsComment, PersonComment,
+    Hobby
+)
+
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Person)
+admin.site.register(HotelOwner)
+admin.site.register(Profile)
+admin.site.register(Hotel)
+admin.site.register(BookInfo)
+admin.site.register(HotelsComment)
+admin.site.register(PersonComment)
+admin.site.register(Hobby)
--- src/booking_app/models.py
+++ src/booking_app/models.py
@@ -1,31 +1,129 @@
 from django.db import models
 
-# Create your models here.
 
-
-class Person(models.Model):
+class User(models.Model):
     SEX_PERSON = {
         "m": "male",
         "f": "female",
     }
     first_name = models.CharField(max_length=30, null=True)
-    last_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, null=True)
     age = models.PositiveIntegerField()
-    sex = models.CharField(max_length=1, choices=SEX_PERSON)
-    email = models.EmailField(null=True)
+    sex = models.CharField(max_length=1, choices=SEX_PERSON, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} {self.email}"
+        return f"{self.first_name} {self.last_name}"
 
-#
-# class Hotels(models.Model):
-#     name = models.CharField(max_length=40)
-#     stars = models.IntegerField()
-#     address = models.CharField(max_length=40)
-#     city = models.CharField(max_length=40)
-#     phone = models.CharField()
-#
-#     def __str__(self):
-#         return f"{self.name}"
+
+class Person(User):
+    guest_rating = models.IntegerField(null=True)
+
+
+class HotelOwner(User):
+    owner_exp_status = models.IntegerField(null=True)
+
+
+class Profile(models.Model):
+    photo = models.ImageField(null=True, blank=True)
+    id_card_number = models.ImageField(null=True)
+    serial = models.CharField(max_length=30, null=True)
+    person_id = models.OneToOneField(
+        to="User",
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="profile"
+    )
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=40, null=True)
+    address = models.CharField(max_length=40, null=True)
+    stars = models.IntegerField(null=True)
+    rating = models.FloatField(null=True)
+    owners = models.ForeignKey(
+        to="HotelOwner",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="hotels",
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class BookInfo(models.Model):
+    detail = models.CharField(max_length=200, null=True)
+    book_time = models.DateTimeField(auto_now_add=True)
+    persons = models.ForeignKey(
+        to="Person",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='booking_info'
+
+    )
+    hotels = models.ForeignKey(
+        to="Hotel",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='booking_info'
+    )
+
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=200, null=True)
+    comment_time = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class HotelsComment(Comment):
+    hotel_rating = models.PositiveIntegerField(null=True)
+    hotels = models.ForeignKey(
+        to="Hotel",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="hotel_comments"
+    )
+    persons = models.ForeignKey(
+        to="Person",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="hotel_comments"
+    )
+
+    def __str__(self):
+        return f"{self.comment}"
+
+
+class PersonComment(Comment):
+    person_rating = models.PositiveIntegerField(null=True)
+    hotels = models.ForeignKey(
+        to="Hotel",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="person_comments"
+    )
+    persons = models.ForeignKey(
+        to="Person",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="person_comments"
+    )
+
+    def __str__(self):
+        return f"{self.comment}"
+
+
+class Hobby(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    detail = models.CharField(max_length=200, null=True)
+    owners = models.ManyToManyField(
+        to="User",
+        related_name="hobbies"
+    )
+
+    def __str__(self):
+        return f" {self.name}"
